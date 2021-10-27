40e22917fad4cf65e0e9076c057fbfbfb33c32e8
pamsterkman <92916045+pamsterkman@users.noreply.github.com>
2021-10-27 11:48:36 +0200

    nbodycpp
    nbodycpp

--- time_nbody.py
+++ time_nbody.py
@@ -4,9 +4,10 @@ import nbody as nbody
 
 start_time = time.perf_counter()
 for i in range(int(sys.argv[1])):
-    nbody.main(i)
+    nbody.main(1)
+    for body, (r, v, m) in nbody.BODIES.items():
+        why = "to also take into account the time of this step"
+
 end_time = time.perf_counter()
 
-print(f"Start Time: {start_time}")
-print(f"End Time: {end_time}")
-print(f"Execution Time: {end_time - start_time:0.6}")
+print("Execution Time: {}".format(end_time - start_time))
