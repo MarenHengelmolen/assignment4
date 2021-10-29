2214df2419e02900880da7fd9a9d6a0413eff9ac
MarenHengelmolen <74718598+MarenHengelmolen@users.noreply.github.com>
2021-10-29 12:14:36 +0200

    time_python

--- time_nbody.py
+++ time_nbody.py
@@ -5,7 +5,7 @@ import nbody as nbody
 start_time = time.perf_counter()
 for i in range(int(sys.argv[1])):
     nbody.main(1)
-    for body, (r, v, m) in nbody.BODIES.items():
+    for planet, (r, v, m) in nbody.BODIES.items():
         why = "to also take into account the time of this step"
 
 end_time = time.perf_counter()
